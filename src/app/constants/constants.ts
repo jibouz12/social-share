847dc2af83bbf3c70c56bbbea537fe4592607957
jb <jb.tournez@gmail.com>
2022-12-20 14:36:53 +0100

    e

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -5,6 +5,6 @@ import { Injectable } from '@angular/core';
 })
 
 export class Constants {
-    public protocol = "http";
+    public protocol = "https";
     public domain = "back-jibouz12.vercel.app";
 }
